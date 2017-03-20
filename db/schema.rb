feabe8960c85043b8020ae6e2bc84bd08037cc36
Doug Eisner <doug.eisner@gmail.com>
2017-03-19 22:36:28 -0400

    added summary and detailed description to products table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170317182340) do
+ActiveRecord::Schema.define(version: 20170320002454) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,8 +58,10 @@ ActiveRecord::Schema.define(version: 20170317182340) do
     t.string   "institution"
     t.string   "category"
     t.decimal  "price"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+    t.text     "summary"
+    t.text     "detailed_description"
   end
 
   create_table "users", force: :cascade do |t|
