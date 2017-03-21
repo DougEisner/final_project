5d76803f7c30a853041f38f635903890872d1414
Marliana Lara <marliana.lara@gmail.com>
2017-03-21 11:50:38 -0400

    Update splash image

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do |n|
+  Product.find_or_create_by(
+  title: "Title ##{n}",
+  developer: "Person",
+  email: "email@mail.com",
+  institution: "School",
+  category: "chemistry",
+  price: 100.99,
+  summary: "Ipsum Lorem Ipsum Lorem",
+  detailed_description: "Stuff stuff stuff details details description"
+  )
+end
