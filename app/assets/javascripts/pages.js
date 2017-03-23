12285c26237eac19b7c4f3369b1fc65586aab5d1
Marliana Lara <marliana.lara@gmail.com>
2017-03-23 00:48:52 -0400

    Add login link to registration page

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,4 +1,4 @@
-$(document).on('turbolinks:load', function() {
+$(document).ready(function() {
   $('.parallax').parallax();
   $(".dropdown-button").dropdown();
   $(".button-collapse").sideNav();
--- app/assets/javascripts/registration.js
+++ app/assets/javascripts/registration.js
@@ -1,4 +1,4 @@
-$(document).on('turbolinks:load', function() {
+$(document).ready(function() {
   var $phone = $('#user_contact_phone').parent();
   var $organization = $('#user_organization').parent();
   var $entity = $('#user_entity_size').parents('.input-field');
@@ -14,7 +14,6 @@ $(document).on('turbolinks:load', function() {
 
   function toggleRole(e) {
     if (e.target.tagName == "INPUT") return;
-
     elements.forEach(function(el) {
       el.val("");
       el.addClass("hide");
