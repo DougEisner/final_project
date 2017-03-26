6949eb04a96f8f1b3428a6aef91635ed5b01ee5b
Marliana Lara <marliana.lara@gmail.com>
2017-03-26 14:47:35 -0400

    Enable submit button when form inputs are completed by user

---
+++ app/assets/javascripts/license.js
@@ -0,0 +1,14 @@
+$(document).on('DOMContentLoaded', function() {
+  const $license = $('#new_license')
+  const $addressLicense = $('#license_address')[0];
+  const $acceptLicense = $('#license_accept')[0];
+  const $submitLicense = $('.submit-license');
+
+  $license.change(function() {
+    if ( $addressLicense.value !== "" && $acceptLicense.checked) {
+      $submitLicense.removeClass('disabled');
+    } else {
+      $submitLicense.addClass('disabled');
+    }
+  });
+});
