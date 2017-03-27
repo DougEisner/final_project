bd9fc0d66f638187d8b6cd0d172953daabd38a4d
Doug Eisner <doug.eisner@gmail.com>
2017-03-27 13:38:58 -0400

    terminology on home page

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
   has_many :licenses
   has_many :CTD_downloads
 
-  has_attached_file :image, styles: { medium: "100x100" }
+  has_attached_file :image, styles: { large: "100x100" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
   def self.search(search)
@@ -20,11 +20,11 @@ class Product < ApplicationRecord
   #                  s3_region: 'us-east-1'
   # validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
-  def s3_credentials
-   {
-     bucket: ENV['S3_PRODUCTS_BUCKET'],
-     access_key_id: ENV['S3_ACCESS_KEY_ID'],
-     secret_access_key: ENV['S3_SECRET_ACCESS_KEY']
-   }
- end
+ #  def s3_credentials
+ #   {
+ #     bucket: ENV['S3_PRODUCTS_BUCKET'],
+ #     access_key_id: ENV['S3_ACCESS_KEY_ID'],
+ #     secret_access_key: ENV['S3_SECRET_ACCESS_KEY']
+ #   }
+ # end
 end
