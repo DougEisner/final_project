d5732b23d108daf7893e20567acc84378a1b4c50
Marliana Lara <marliana.lara@gmail.com>
2017-03-28 16:11:09 -0400

    expose custom header

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,7 +3,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
    origins '*'
    resource '*',
      headers: :any,
-     expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+     expose: ['access-token', 'expiry', 'token-type', 'uid', 'client', '_code_alliance_reg'],
      methods: [:get, :post, :put, :patch, :delete, :options, :head]
  end
 end
